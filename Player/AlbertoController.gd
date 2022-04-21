6e50ac7fb7f8e7b0a45c386a5f16522089fa8fb1
Alberto Pérez Fructuoso <albertoperez_135@outlook.es>
2022-04-21 23:30:45 +0200

    Main Commit

---
+++ Player/AlbertoController.gd
@@ -0,0 +1,41 @@
+extends KinematicBody
+
+# Variables
+export var speed = 10
+export var acceleration = 5
+export var gravity = 0.98
+export var jumpPower = 30
+export var mouseSensivility = 0.3
+
+onready var head = $Head
+onready var camera = $Head/Camera
+
+var velocity = Vector3()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(deg2rad(-event.relative.x * mouseSensivility))
+		camera.rotate_x(deg2rad(-event.relative.y * mouseSensivility))
+func _physics_process(delta):
+	
+	var headBasic = head.get_global_transform().basis
+	var direction = Vector3()
+	
+	if Input.is_action_pressed("wKey"):
+		direction -= headBasic.z
+		
+	elif Input.is_action_pressed("sKey"):
+		direction += headBasic.z
+		
+		
+	elif Input.is_action_pressed("aKey"):
+		direction -= headBasic.x
+	
+	elif Input.is_action_pressed("dKey"):
+		direction += headBasic.x
+	
+		direction = direction.normalized()
+		
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+
+	velocity = move_and_slide(velocity)
