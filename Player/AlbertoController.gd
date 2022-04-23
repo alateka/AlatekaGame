f78193900d2a2bb0b8876a678a1cfd6ba27fe241
ALATEKA-WPC <alateka@vivaldi.net>
2022-04-23 13:48:40 +0200

    Modified AlbertoController.gd

--- Player/AlbertoController.gd
+++ Player/AlbertoController.gd
@@ -12,30 +12,33 @@ onready var camera = $Head/Camera
 
 var velocity = Vector3()
 
+
+# Camera Controller
 func _input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(deg2rad(-event.relative.x * mouseSensivility))
 		camera.rotate_x(deg2rad(-event.relative.y * mouseSensivility))
+
+
+# Movement Controller
 func _physics_process(delta):
-	
+
 	var headBasic = head.get_global_transform().basis
 	var direction = Vector3()
-	
+
+
 	if Input.is_action_pressed("wKey"):
 		direction -= headBasic.z
-		
-	elif Input.is_action_pressed("sKey"):
-		direction += headBasic.z
-		
-		
-	elif Input.is_action_pressed("aKey"):
-		direction -= headBasic.x
-	
-	elif Input.is_action_pressed("dKey"):
-		direction += headBasic.x
-	
-		direction = direction.normalized()
-		
-	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 
+	if Input.is_action_pressed("sKey"):
+		direction += headBasic.z
+
+	if Input.is_action_pressed("aKey"):
+		direction -= headBasic.x
+
+	if Input.is_action_pressed("dKey"):
+		direction += headBasic.x
+
+
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	velocity = move_and_slide(velocity)
