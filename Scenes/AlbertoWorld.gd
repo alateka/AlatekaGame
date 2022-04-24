5b389272dd7e279758b23e847e4571cbdfbb12fc
ALATEKA-WPC <alateka@vivaldi.net>
2022-04-24 22:16:45 +0200

    Added mouse icon

--- Scenes/AlbertoWorld.gd
+++ Scenes/AlbertoWorld.gd
@@ -6,7 +6,3 @@ func _ready():
 
 	# It changes to mouse capture mode
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-	# It modify game window 
-	OS.set_window_size(Vector2(1280, 720))
-	OS.center_window()
